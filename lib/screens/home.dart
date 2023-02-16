6f163a80095322a6a9a97248eb7c7a881b909de0
ikyrie <mah.c5r@gmail.com>
2023-02-16 11:51:23 -0300

    Montando a página de checkout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:panucci_delivery/components/item_list.dart';
+import 'package:panucci_delivery/screens/checkout.dart';
 import 'package:panucci_delivery/store/carrinho_store.dart';
 import 'package:provider/provider.dart';
 import '../components/categoria_text.dart';
@@ -14,6 +15,7 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final carrinhoStore = Provider.of<CarrinhoStore>(context, listen: false);
+    final BuildContext homeContext = context;
     return SafeArea(
       child: Scaffold(
         body: CustomScrollView(
@@ -40,60 +42,68 @@ class Home extends StatelessWidget {
               hasScrollBody: false,
               child: Align(
                 alignment: Alignment.bottomCenter,
-                child: Container(
-                    width: double.infinity,
-                    height: 80,
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.surfaceTint,
-                        borderRadius:
-                            const BorderRadius.vertical(top: Radius.circular(10))),
-                    child: Stack(
-                        children: [
-                          Align(
-                            alignment: Alignment.centerLeft,
-                            child: Row(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.only(right: 8.0),
-                                  child: Text(
-                                    carrinhoStore.totalItemsInBasket.toString(),
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: 
-                                        Theme.of(context).colorScheme.onPrimary),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return Checkout(homeContext: homeContext,);
+                    }));
+                  },
+                  child: Ink(
+                      width: double.infinity,
+                      height: 80,
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.surfaceTint,
+                          borderRadius:
+                              const BorderRadius.vertical(top: Radius.circular(10))),
+                      child: Stack(
+                          children: [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: Row(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 8.0),
+                                    child: Text(
+                                      carrinhoStore.totalItemsInBasket.toString(),
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                        color: 
+                                          Theme.of(context).colorScheme.onPrimary),
+                                    ),
                                   ),
-                                ),
-                                Icon(
-                                  Icons.shopping_basket_outlined,
-                                  size: 24,
+                                  Icon(
+                                    Icons.shopping_basket_outlined,
+                                    size: 24,
+                                    color:
+                                        Theme.of(context).colorScheme.onPrimary,
+                                  )
+                                ],
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.center,
+                              child: Text(
+                                "Ver carrinho",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: 
+                                    Theme.of(context).colorScheme.onPrimary),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Text(
+                                "R\$ ${carrinhoStore.totalCost.toStringAsFixed(2)}",
+                                style: TextStyle(
+                                  fontSize: 16,
                                   color:
-                                      Theme.of(context).colorScheme.onPrimary,
-                                )
-                              ],
+                                    Theme.of(context).colorScheme.onPrimary),
+                              ),
                             ),
-                          ),
-                          Align(
-                            alignment: Alignment.center,
-                            child: Text(
-                              "Ver carrinho",
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: 
-                                  Theme.of(context).colorScheme.onPrimary),
-                            ),
-                          ),
-                          Align(
-                            alignment: Alignment.centerRight,
-                            child: Text(
-                              "R\$ ${carrinhoStore.totalCost.toStringAsFixed(2)}",
-                              style: TextStyle(
-                                fontSize: 16,
-                                color:
-                                  Theme.of(context).colorScheme.onPrimary),
-                            ),
-                          ),
-                        ])),
+                          ])),
+                ),
               ),
             )
             : const SliverToBoxAdapter())
